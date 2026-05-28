3b89007cef42e1077485332dc361f97529d710a4
chungpt <it.chungpt1291@gmail.com>
2026-05-29 00:35:13 +0700

    feat: implement Posts remote blog app with markdown reading

---
+++ packages/posts/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PostsApp from './PostsApp';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <div className="p-4 w-[500px]">
+        <PostsApp />
+      </div>
+    </QueryClientProvider>
+  </React.StrictMode>
+);

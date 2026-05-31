a2b599fdf92562d3c82c16c9f01b0e2bf6be0c0e
prxxie <prxxie@users.noreply.github.com>
2026-05-31 15:19:31 +0000

    deploy: 2b08bd0f928d1fc5671438fb63b72bef56af1920

--- mfe/sokoban/assets/index-DH8gDd4U.js
+++ mfe/sokoban/assets/index-DH8gDd4U.js
@@ -1,5 +1,5 @@
 import { importShared } from './__federation_fn_import-D-nfbenS.js';
-import SokobanApp, { j as jsxRuntimeExports } from './__federation_expose_SokobanApp-KTKXZlwF.js';
+import SokobanApp, { j as jsxRuntimeExports } from './__federation_expose_SokobanApp-CN4-_hGq.js';
 import { r as reactDomExports } from './index-D9Af7wOI.js';
 
 true&&(function polyfill() {

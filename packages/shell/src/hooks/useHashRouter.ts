1e76dd9650ba981d52e8075ea4db11acb28beaaf
chungpt <it.chungpt1291@gmail.com>
2026-06-02 23:41:50 +0700

    feat: fix slitherlink routing on first click and fix test warnings

--- packages/shell/src/hooks/useHashRouter.ts
+++ packages/shell/src/hooks/useHashRouter.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import type { Tab } from "../types";
 
-const VALID_TABS: Tab[] = ["home", "about", "posts", "shikaku", "sokoban"];
+const VALID_TABS: Tab[] = ["home", "about", "posts", "shikaku", "sokoban", "slitherlink"];
 
 function getTabFromHash(): Tab {
   const hash = typeof window !== "undefined" ? window.location.hash : "";
